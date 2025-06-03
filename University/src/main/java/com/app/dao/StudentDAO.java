4526fc11e6b7cebe9cae3eb914edf87eaa325f08
Digvijay Devare <136898769+Jaydevare@users.noreply.github.com>
2025-06-03 21:52:58 +0530

    Update StudentDAO.java

--- University/src/main/java/com/app/dao/StudentDAO.java
+++ University/src/main/java/com/app/dao/StudentDAO.java
@@ -14,7 +14,7 @@ public class StudentDAO
 	{
 		Connection con = null;
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db2","root", "Jay78@devare");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db2","root", "e");
 		return con;
 	}
 	
